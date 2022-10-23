3ff1123f1b1bf58cd8446210304c6200684ad453
appuser <appuser@fastmail.com>
2022-10-24 00:01:12 +1300

    switch order of emphasis to correct

--- utils/replaceEmphasis.jsx
+++ utils/replaceEmphasis.jsx
@@ -1,7 +1,7 @@
 const replaceEmphasis = (str) => {
     // Replaces _emphasized_ passages with css stylized span
     const passageArray = str.split("_").map((passage, idx) => {
-        return <span key={idx} className={idx % 2 !== 0 ? '' : 'text-purple-800'}>{passage}</span>
+        return <span key={idx} className={idx % 2 === 0 ? '' : 'text-purple-800'}>{passage}</span>
     })
     return passageArray
 }

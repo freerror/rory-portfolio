685580c165aba6978eb622eec736fe98ddebdfc0
appuser <appuser@fastmail.com>
2022-10-23 23:57:52 +1300

    fix lack of key prop and simplify

--- utils/replaceEmphasis.jsx
+++ utils/replaceEmphasis.jsx
@@ -1,10 +1,7 @@
 const replaceEmphasis = (str) => {
     // Replaces _emphasized_ passages with css stylized span
     const passageArray = str.split("_").map((passage, idx) => {
-        if (idx % 2 !== 0) {
-            return <span className='text-purple-800'>{passage}</span>
-        }
-        return passage
+        return <span key={idx} className={idx % 2 !== 0 ? '' : 'text-purple-800'}>{passage}</span>
     })
     return passageArray
 }

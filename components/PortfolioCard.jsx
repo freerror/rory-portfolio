3a4e57d9f200414c819ccb2144fe869b468fa8f1
appuser <appuser@fastmail.com>
2022-10-24 00:48:46 +1300

    Added site data fixture

--- components/PortfolioCard.jsx
+++ components/PortfolioCard.jsx
@@ -1,14 +1,14 @@
 import Image from 'next/image'
 
-const PortfolioCard = ({ title, url, img }) => (
+const PortfolioCard = (props) => (
     <div>
         <h3 className='text-xl pb-3'>
-            {title}
+            {props.title}
         </h3>
-        <a href={url}>
+        <a href={props.url}>
             <div className='basis-full shadow-xl rounded-2xl border overflow-hidden'>
                 <div className='h-fit overflow-hidden flex flex-shrink'>
-                    <Image src={img} alt={"Images of " + title} />
+                    <Image src={props.img} alt={"Images of " + props.title} />
                 </div>
             </div>
         </a>
--- pages/index.js
+++ pages/index.js
@@ -16,10 +16,11 @@ import DetailCard from '../components/DetailCard'
 import PortfolioSection from '../components/PortfolioSection'
 import PortfolioCard from '../components/PortfolioCard'
 
+import siteData from '../fixtures/siteData.json'
+
 export default function Home() {
     const [dark, setDark] = useState("")
     const [mainImg, setMainImg] = useState(thumbsLight)
-
     const detailSection = useRef(null)
 
     function handleScrollClick() {
@@ -42,72 +43,26 @@ export default function Home() {
     return (
         <div className={dark}>
             <Head>
-                <title>Rory Coded This</title>
-                <meta name="description" content="Rory's website and portfolio - Rory coded this" />
-                <meta name="author" content="Rory Samson" />
-                <meta name="keywords" content="HTML, CSS, Javascript, Portfolio, Rory Samson" />
+                <title>{siteData.title}</title>
+                <meta name="description" content={siteData.description} />
+                <meta name="author" content={siteData.author} />
+                <meta name="keywords" content={siteData.keywords} />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
             <main className='bg-white dark:bg-stone-900 dark:text-white font-asap transition'>
                 <NavBar {...{ toggleDark }} />
-                <PageIntro {...{
-                    title: "Rory Samson",
-                    subtitle: "Front-to-back developer and IT business analyst",
-                    para: "Hi, _I'm Rory_.\n\nI am looking for my _next developer job_. Please _check out my work_.\nCould I be a good fit for your team?",
-                    mainImg,
-                    handleScrollClick
-                }} />
+                <PageIntro {...{ ...siteData.intro, mainImg, handleScrollClick }} />
                 <div ref={detailSection} className="h-24"></div>
-                <DetailCardSection {...{
-                    title: "What I can do",
-                    para: "I am a _developer_ and competent _operations_ _analyst_ with a passion for\nGNU/Linux, FOSS, art and design.\n\nCheck out my CV to learn more.\nHere is a taste:",
-                }} >
-                    <DetailCard {...{
-                        title: "Develop and Automate",
-                        img: code,
-                        body: "I design and maintain _python automation_ for a fast-paced enterprise service centre\nand build _apps and websites_ on proven modern frameworks.",
-                        detailHeading: "Dev tools I use",
-                        detailPara: [
-                            "Javascript, HTML and CSS",
-                            "Python",
-                            "Next.js",
-                            "React",
-                            "Jest",
-                            "C++",
-                            "GNU/Linux!"
-                        ]
-                    }} />
-                    <DetailCard {...{
-                        title: "Illustration and Design",
-                        img: design,
-                        body: "I create _audiovisual, 2D and 3D assets_, with skill in a wide range of tools.",
-                        detailHeading: "Dev tools I use",
-                        detailPara: [
-                            "Inkscape/Illustrator",
-                            "Gimp/Photoshop",
-                            "Blender",
-                            "Penpot",
-                            "DaVinci Resolve",
-                            "Presonus Studio One",
-                            "FL Studio"
-                        ]
-                    }} />
-                    <DetailCard {...{
-                        title: "IT operations analysis",
-                        img: analysis,
-                        body: "I have _years of experience_ as an _IT operations analyst_ for a telco.\nI provide deep _insights_ and effective recommendations.",
-                        detailHeading: "Analytical tools I use",
-                        detailPara: [
-                            "Excel",
-                            "PowerBI",
-                            "Python"
-                        ]
-                    }} />
+                <DetailCardSection {...siteData.detail} >
+                    {siteData.detail.cards.map((card, idx) => (
+                        <DetailCard key={idx} {...card} />
+                    ))}
                 </DetailCardSection>
                 <PortfolioSection title="Portfolio" subtitle="Check out a sample of my latest projects!">
-                    <PortfolioCard {...{ title: "Expense Tracker", img: work1, url: "https://xp3ns.herokuapp.com/" }} />
-                    <PortfolioCard {...{ title: "Decision Maker", img: work2, url: "https://limitless-mountain-91728.herokuapp.com/" }} />
+                    {siteData.portfolio.cards.map((card, idx) => (
+                        <PortfolioCard key={idx} {...card} />
+                    ))}
                 </PortfolioSection>
 
             </main >
